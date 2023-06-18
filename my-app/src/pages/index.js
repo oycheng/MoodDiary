bcb3245e1c1f795610def3afd6ce63c0d4aecb59
ylylime <eming@ucdavis.edu>
2023-06-17 21:06:16 -0700

    update global.css

--- my-app/src/pages/index.js
+++ my-app/src/pages/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import '../index.css';
+import '../global.css';
 
 const Home = () => {
 	return (
 		<div>
-			<h1 className = "title">Welcome to MoodDiary!</h1>
+			<h1>Welcome to MoodDiary!</h1>
 			<h2>
 				Speak your mind and express your emotions on MoodDiary. <br></br>
             	Utilize your webcam and microphone to record your thoughts<br></br>
