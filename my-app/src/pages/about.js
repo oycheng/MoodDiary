6beca3f19c2d42e44c211d4c88c0aa52066c4700
ylylime <eming@ucdavis.edu>
2023-06-17 20:42:39 -0700

    updat

---
+++ my-app/src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h1>
+            Speak, Express, Reflect.
+			</h1>
+            <h2>
+            MoodDiary is a daily mood journal that allows you to express your feelings and emotions through video and spoken words. 
+            With Hume AI, your emotion will be determined from your facial and speech expressions. 
+            It's a personal journey of self-reflection and emotional awareness.
+            </h2>
+		</div>
+	);
+};
+
+export default About;
---
+++ my-app/src/pages/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../index.css';
+
+const Home = () => {
+	return (
+		<div>
+			<h1 className = "title">Welcome to MoodDiary!</h1>
+			<h2>
+				Speak your mind and express your emotions on MoodDiary. <br></br>
+            	Utilize your webcam and microphone to record your thoughts<br></br>
+				and feelings about your day. 
+			</h2>
+		</div>
+	);
+};
+
+export default Home;
