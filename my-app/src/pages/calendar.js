57a2264f228fa970771790b5923979857ea5e9c0
oycheng <123531322+oycheng@users.noreply.github.com>
2023-06-18 00:41:33 -0700

    calendar date color

--- my-app/src/pages/calendar.js
+++ my-app/src/pages/calendar.js
@@ -3,16 +3,35 @@ import '../global.css';
 import '../pages/calendar.css';
 import { useState } from 'react';
 import Calendar from 'react-calendar';
+import { addDays, differenceInCalendarDays } from 'date-fns';
 
+const now = new Date();
+const tomorrow = addDays(now, 1);
+const in3Days = addDays(now, 3);
+const in5Days = addDays(now, 5);
+
+const highlightedDates = [tomorrow, in3Days, in5Days];
+
+function isSameDay(a, b) {
+	return differenceInCalendarDays(a, b) === 0;
+}
 
 const Calendr = () => {
 	const [date, setDate] = useState(new Date());
-
+	
+	function tileClassName({ date, view }) {
+		if (
+		view === 'month' &&
+		highlightedDates.find((dDate) => isSameDay(dDate, date))
+		) {
+			return 'highlight';
+		}
+	}
 	return (
 		<div>
 		<h2>Mood Board</h2>
 		<div className='calendar-container'>
-        	<Calendar onChange={setDate} value={date} />
+        	<Calendar tileClassName={tileClassName} onChange={setDate} value={date} />
       	</div>
 	  </div>
 	);
